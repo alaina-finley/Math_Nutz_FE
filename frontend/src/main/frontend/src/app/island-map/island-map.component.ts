6a4a3d39e3b24567202ae0327c7fee53f0e16b3b
Kyle <kylebeard1996@gmail.com>
2018-04-18 12:57:14 -0600

    Added island locking and completed island functionality

--- frontend/src/main/frontend/src/app/island-map/island-map.component.ts
+++ frontend/src/main/frontend/src/app/island-map/island-map.component.ts
@@ -18,12 +18,12 @@ export class IslandMapComponent implements OnInit {
   title = 'Island Map';
 
   //get progresses from game-progress.service
-  gps: GameProgressService;
-  progresses: Progress[] = this.gps.getProgress();
+  //gps: GameProgressService;
+  //progresses: Progress[] = this.gps.getProgress();
 
   //get users
-  us: UserService;
-  users: Promise<User[]> = this.us.getUsers();
+  // us: UserService;
+  //users: Promise<User[]> = this.us.getUsers();
 
   constructor(
     private auth: AuthenticationService,
@@ -36,11 +36,14 @@ export class IslandMapComponent implements OnInit {
   logout() {
     this.auth.logout();
   }
-  
-  additionIslandsUnlocked(studentId: number) {
-    var unlockedIslands = 0; //0 if only the first island is unlocked, 1 for the 1st & 2nd unlocked, and 2 for all three
 
-    var student = this.progresses.find(p => p.student_id == studentId);
+  /**
+   * Returns 0 if only the first island is unlocked, 1 for the 1st & 2nd unlocked, 2 for all three, and 3 for when all islands are completed
+   */
+  additionIslandsUnlocked() {
+    var unlockedIslands = 0; // return value
+    var us_id = JSON.parse(localStorage.getItem("user")).id;
+    var student = (JSON.parse(localStorage.getItem("progresses"))).find(p => p.student_id == us_id);
     var totalCorrect = student.lev11_correct + student.lev12_correct + student.lev13_correct;
 
     if (totalCorrect >= 10) {// all questions on island 1 are correct
@@ -49,14 +52,17 @@ export class IslandMapComponent implements OnInit {
     if (totalCorrect >= 20) {// all questions on island 2 are correct
       unlockedIslands = 2;
     }
+    if (totalCorrect >= 30) {// all islands are completed
+      unlockedIslands = 3;
+    }
 
     return unlockedIslands;
   }
 
-  subtractionIslandsUnlocked(studentId: number) {
+  subtractionIslandsUnlocked() {
     var unlockedIslands = 0; //0 if only the first island is unlocked, 1 for the 1st & 2nd unlocked, and 2 for all three
-
-    var student = this.progresses.find(p => p.student_id == studentId);
+    var us_id = JSON.parse(localStorage.getItem("user")).id;
+    var student = (JSON.parse(localStorage.getItem("progresses"))).find(p => p.student_id == us_id);
     var totalCorrect = student.lev21_correct + student.lev22_correct + student.lev23_correct;
 
     if (totalCorrect >= 10) {// all questions on island 1 are correct
@@ -65,14 +71,18 @@ export class IslandMapComponent implements OnInit {
     if (totalCorrect >= 20) {// all questions on island 2 are correct
       unlockedIslands = 2;
     }
+    if (totalCorrect >= 30) {// all islands are completed
+      unlockedIslands = 3;
+    }
 
     return unlockedIslands;
   }
 
-  pvIslandsUnlocked(studentId: number) {
+  pvIslandsUnlocked() {
     var unlockedIslands = 0; //0 if only the first island is unlocked, 1 for the 1st & 2nd unlocked, and 2 for all three
 
-    var student = this.progresses.find(p => p.student_id == studentId);
+    var us_id = JSON.parse(localStorage.getItem("user")).id;
+    var student = (JSON.parse(localStorage.getItem("progresses"))).find(p => p.student_id == us_id);
     var totalCorrect = student.lev31_correct + student.lev32_correct + student.lev33_correct;
 
     if (totalCorrect >= 10) {// all questions on island 1 are correct
@@ -80,6 +90,9 @@ export class IslandMapComponent implements OnInit {
     }
     if (totalCorrect >= 20) {// all questions on island 2 are correct
       unlockedIslands = 2;
+    }
+    if (totalCorrect >= 30) {// all islands are completed
+      unlockedIslands = 3;
     }
 
     return unlockedIslands;
