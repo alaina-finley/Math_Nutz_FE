aa47d4b6b4290e881663d62be30b62073ef57710
Ash Grevelink <agrev8@gmail.com>
2018-04-29 19:39:45 -0600

    Cole final changes

---
+++ frontend/src/main/frontend/src/app/home-level/home-level.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { GameProgressService } from '../progress/game-progress.service';
+
+@Component({
+  selector: 'app-home-level',
+  templateUrl: './home-level.component.html',
+  styleUrls: ['./home-level.component.css']
+})
+export class HomeLevelComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private gameProgressService: GameProgressService,
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+  returnMap(){
+    console.log("excellent");
+    this.router.navigate(['../islandMap']);
+  }
+
+  shouldDisplay(diff: number, oper: number){
+    // var sr = (JSON.parse(localStorage.getItem("progresses"))).find(p => p.student_id == studentId);
+    let idNo = JSON.parse(localStorage.getItem("user")).id;
+    let records = JSON.parse(localStorage.getItem("progresses"));
+    return (this.gameProgressService.findCompleted(idNo, records, diff, oper) >= 10)
+  }
+
+}
